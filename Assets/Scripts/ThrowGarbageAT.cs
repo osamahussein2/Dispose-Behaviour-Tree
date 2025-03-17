3cefc5fec913d1b5ca5aaad186d3abf26807751e
Osama Hussein <osamahusseinn2@gmail.com>
2025-03-17 12:07:58 -0400

    Garbage is thrown into the bin

--- Assets/Scripts/ThrowGarbageAT.cs
+++ Assets/Scripts/ThrowGarbageAT.cs
@@ -16,6 +16,12 @@ namespace NodeCanvas.Tasks.Actions {
 
         public BBParameter<Vector3> cameraOffset;
 
+        private GameObject topOfBin;
+
+        public BBParameter<float> garbageFallSpeed;
+
+        private bool isGarbageFallingIntoBin;
+
         //Use for initialization. This is called only once in the lifetime of the task.
         //Return null if init was successfull. Return an error string otherwise
         protected override string OnInit() {
@@ -29,6 +35,15 @@ namespace NodeCanvas.Tasks.Actions {
 		{
             // Set the private gameobject to find the garbage somewhere in the scene
             spawnedGarbage = GameObject.FindWithTag("Garbage");
+
+            // Get the child of the garbage bin object to set the top bin
+            topOfBin = garbageBin.value.transform.GetChild(0).gameObject;
+
+            // Set the top bin to be positioned just above the top of garbage bin
+            topOfBin.transform.localPosition = new Vector3(0.0f, 0.65f, 0.0f);
+
+            // Set the bool to false first to ensure the top bin doesn't lower
+            isGarbageFallingIntoBin = false;
 
             // However, end this action if the garbage object isn't found in the scene
             if (!spawnedGarbage)
@@ -53,19 +68,26 @@ namespace NodeCanvas.Tasks.Actions {
                 // Stop moving the garbage collector
                 agent.transform.position += new Vector3(0.0f, 0.0f, 0.0f);
 
-                // Move the spawned garbage towards the garbage bin to throw out
-                spawnedGarbage.transform.position += (garbageBin.value.transform.position - 
-                    spawnedGarbage.transform.position) * Time.deltaTime;
-
                 // Decrease the spawned garbage scale to make sure it's not longer than the garbage bin itself
                 spawnedGarbage.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
 
-                // If the spawned garbage and garbage bin are close to each other
-                if (Vector3.Distance(spawnedGarbage.transform.position, garbageBin.value.transform.position) <= 0.2f)
+                // Move the spawned garbage towards the top of garbage bin
+                spawnedGarbage.transform.position += (topOfBin.transform.position - spawnedGarbage.transform.position) 
+                    * Time.deltaTime;
+
+                // If the spawned garbage and the top of garbage bin are close to each other
+                if (Vector3.Distance(spawnedGarbage.transform.position, topOfBin.transform.position) <= 0.1f)
                 {
-                    // Destroy the spawned garbage and end this action
-                    Object.Destroy(spawnedGarbage);
-                    EndAction(true);
+                    // Set bool to true to make the garbage fall into the bin
+                    isGarbageFallingIntoBin = true;
+
+                    // Once the top bin goes less than 0 in y position, destroy the spawned garbage
+                    if (topOfBin.transform.position.y <= 0f)
+                    {
+                        // End this action once the garbage has been destroyed
+                        Object.Destroy(spawnedGarbage);
+                        EndAction(true);
+                    }
                 }
             }
 
@@ -74,6 +96,19 @@ namespace NodeCanvas.Tasks.Actions {
                 // Move towards the garbage bin after picking up the garbage from the last action
                 agent.transform.position += (garbageBin.value.transform.position - agent.transform.position)
                     * moveToGarbageBinSpeed.value * Time.deltaTime;
+            }
+
+            // Make sure the top bin doesn't move when either the bool is false or is less than 0 in y position
+            if (!isGarbageFallingIntoBin || topOfBin.transform.position.y <= 0f)
+            {
+                topOfBin.transform.position += Vector3.zero;
+            }
+
+            // Otherwise, lower top bin to act as if the garbage is thrown into the bin
+            else
+            {
+                topOfBin.transform.position += (garbageBin.value.transform.position - topOfBin.transform.position) 
+                    * garbageFallSpeed.value * Time.deltaTime;
             }
         }
 
